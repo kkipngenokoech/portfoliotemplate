7937353adaeb23730c984552bb460f5b4b600e8c
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-08-30 12:15:55 +0300

    updated the link to my blogs

--- assets/js/app.js
+++ assets/js/app.js
@@ -28,7 +28,7 @@ let header = $(`
    <li class="nav-item nav-item-hover"><a class="nav-link" href="experience.html">Experience</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="techstack.html">techstack</a></li>
    <li class="nav-item nav-item-hover"><a class="nav-link" href="design.html">Gallery</a></li>
-   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://hashnode.com/" target="_blank">Blogs</a></li>
+   <li class="nav-item nav-item-hover"><a class="nav-link" href="https://medium.com/@kkipngenokoech" target="_blank">Blogs</a></li>
    <li class="nav-item">
    <input type="checkbox" id="dark_toggler" class="dark_toggler" aria-label="Toggle Light Mode" onclick="toggle_light_mode()" checked>
    </li>
