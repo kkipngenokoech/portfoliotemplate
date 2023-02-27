01440bf76616e4477183d9567b026efb2b17b7df
kkipngenokoech <kkipngenokoech22@gmail.com>
2023-02-27 22:33:26 +0300

    adding projects section

--- assets/js/project.js
+++ assets/js/project.js
@@ -101,13 +101,13 @@ const projects = [
     Githublink: "",
   },
   {
-    title: "Music Recommendation Engine",
-    cardImage: "assets/images/project-page/music.jpg",
-    description: "Built using Python.",
+    title: "Simple Node Server",
+    cardImage: "assets/images/project-page/nodeJS.png",
+    description: "Built using node js.",
     tagimg:
       "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
     Previewlink: "",
-    Githublink: "",
+    Githublink: "https://github.com/kkipngenokoech/node-web-server",
   },
 ];
 
