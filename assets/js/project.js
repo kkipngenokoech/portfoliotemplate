0b2864fc41dd7f56e63a075a8cff3a2fbd78fbfc
kkipngenokoech <kkipngenokoech22@gmail.com>
2023-03-05 14:38:17 +0300

    adding local library

--- assets/js/project.js
+++ assets/js/project.js
@@ -83,13 +83,13 @@ const projects = [
     Githublink: "",
   },
   {
-    title: "Battery Life Calculator",
-    cardImage: "assets/images/project-page/battery.png",
-    description: "Built battery life calculator using Python.",
+    title: "knls",
+    cardImage: "assets/images/project-page/localLibrary.jpg",
+    description: "A local library website built with node js",
     tagimg:
       "https://camo.githubusercontent.com/888e388801f947dec7c3d843942c277af25fe2b1aed1821542c4e711f210312a/68747470733a2f2f75706c6f61642e77696b696d656469612e6f72672f77696b6970656469612f636f6d6d6f6e732f7468756d622f632f63332f507974686f6e2d6c6f676f2d6e6f746578742e7376672f37363870782d507974686f6e2d6c6f676f2d6e6f746578742e7376672e706e67",
-    Previewlink: "",
-    Githublink: "",
+    Previewlink: "https://knls.onrender.com/",
+    Githublink: "https://github.com/kkipngenokoech/knls",
   },
   {
     title: "Simple Express Server",
